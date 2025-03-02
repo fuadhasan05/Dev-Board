f65ae1264a3d8338fcbcc8f6844e808c32f0b9fd
Fuad Hasan <fuadhasan3503@gmail.com>
2025-03-02 23:20:41 +0600

    added script file

---
+++ script/app.js
@@ -0,0 +1,54 @@
+const boardCards = document.querySelectorAll('.card-btn'); 
+//Added date
+const today = new Date();
+const formateDate = today.toDateString();
+document.getElementById('date').innerText = formateDate;
+
+
+for (const boardCard of boardCards) {
+    boardCard.addEventListener('click', function(event){
+        alert('Board updated Successfully')
+        const taskNumber= getInnerTextByID('task-number');
+        const convertTaskNumber = parseInt(taskNumber);
+        const taskSum = convertTaskNumber - 1;
+        setInnerTextByIdAndValue('task-number', taskSum );
+
+        const taskAdd = getInnerTextByID('task-add');
+        const sum = parseInt(taskAdd) + 1;
+        setInnerTextByIdAndValue( 'task-add', sum);
+
+        const date = new Date();
+        const localTime = date.toLocaleTimeString();
+
+        //added title in the cache
+        const parentCard = event.target.parentNode.parentNode ;
+        console.log(parentCard);
+        const title = parentCard.querySelector('.title').innerText;
+
+        const cache = document.getElementById('cache');
+        const div = document.createElement('div');
+        div.innerHTML = `<p class="text-base tracking-wide bg-[#F4F7FF] rounded-lg p-4 mt-5"> You have Complete The Task ${title} at ${localTime} </p>`;
+        cache.appendChild(div)
+
+
+        //  History Clear
+        document.getElementById('clear-history').addEventListener('click', function(){
+            cache.innerText = '';
+        })
+
+        
+       this.disabled = true;
+
+       if (convertTaskNumber === 1) {
+            alert('Congrats!! You have completed all the current task');
+       }
+       
+    })
+    
+}
+
+
+
+
+
+
